62fd107fdbb0e88226d183ebece24ba1756af473
sempaiKakashi <33700552+sempaiKakashi@users.noreply.github.com>
2018-01-03 19:05:45 +0300

    Лишній скріпт

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,1 @@
-console.log("Hello World!")
-$(document).ready(function (){
-    	$(".topButton").mouseover(function(){
-    		/*$(this).css("backgroundColor", "greenyellow");*/
-    		$(".topButton").fadeIn("slow")
-
-    })
-    	.mouseout(function(){
-    		/*/**$(this).css("backgroundColor", "#50C3AF")/*/
-    	});
-});
+console.log("Hello World!");
