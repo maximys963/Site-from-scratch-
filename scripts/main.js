c2ba08d5b1976372d63fe0e05dbea2ce44a969ac
Maxim <maxrap7878@mail.ru>
2018-01-10 19:06:24 +0200

    add eventlistener for left button

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+// var thirdButtons = document.getElementsByClassName("third-button");
+// thirdButtons.addEventListener("mouseover", ChangeColor);
+// function ChangeColor (){
+//     for(var i=0; i<thirdButtons.length; i++){
+//         thirdButtons[i].style.backgroundColor = "greenyellow";
+//     }
+//
+// }
+//
+
+
+
+
+
+var button1 = document.getElementById("btn1");
+button1.addEventListener("mouseover", Greeting);
+button1.addEventListener("mouseout",Back);
+function Greeting(){
+    console.log("hello btn");
+    button1.style.backgroundColor = "greenyellow"
+}
+function Back(){
+    console.log("hello btn");
+    button1.style.backgroundColor = "#50C3AF"
+}
