46107806cb939188bf194d9f71948eed580934c4
Maxim <maxrap7878@mail.ru>
2018-01-10 19:28:03 +0200

    fixed eventlister

--- scripts/main.js
+++ scripts/main.js
@@ -15,11 +15,26 @@
 var button1 = document.getElementById("btn1");
 button1.addEventListener("mouseover", Greeting);
 button1.addEventListener("mouseout",Back);
+button1.addEventListener("mousedown",OnMousedown);
+button1.addEventListener("mouseup",OnMouseup);
 function Greeting(){
     console.log("hello btn");
-    button1.style.backgroundColor = "greenyellow"
+    button1.style.backgroundColor = "#ADFF2F"
+    button1.style.transitionDuration = "0.6s";
+
 }
 function Back(){
     console.log("hello btn");
     button1.style.backgroundColor = "#50C3AF"
+    button1.style.transitionDuration = "0.6s";
+}
+function OnMousedown(){
+    console.log("hello btn");
+    button1.style.backgroundColor = "#98FB98"
+    button1.style.transitionDuration = "0.1s";
+}
+function OnMouseup () {
+    console.log("hello btn");
+    button1.style.backgroundColor = "#ADFF2F"
+    button1.style.transitionDuration = "0.5s";
 }
