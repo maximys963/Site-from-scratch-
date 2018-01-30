41bd41058530e59685dddda9fb76a760b451dc72
ratkin <ratkin@prodex.net.ua>
2018-01-30 15:59:21 +0200

    Допилив анімацію для кнопок

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-var buttons = document.getElementsByClassName("third-button");
+var button_1 = document.getElementsByClassName("third-button");
 // button це типу масив в який зібрав всі ноди (теги) з классом third-button
-for(var i=0; i<buttons.length; i++){
+for(var i=0; i<button_1.length; i++){
 
-    buttons[i].addEventListener("mouseover", Greeting);
-    buttons[i].addEventListener("mouseout",Back);
-    buttons[i].addEventListener("mousedown",OnMousedown);
-    buttons[i].addEventListener("mouseup",OnMouseup);
+    button_1[i].addEventListener("mouseover", Greeting);
+    button_1[i].addEventListener("mouseout",Back);
+    button_1[i].addEventListener("mousedown",OnMousedown);
+    button_1[i].addEventListener("mouseup",OnMouseup);
 
 // навішую на кожну ноду обработчик событий
 }
@@ -28,6 +28,38 @@ function OnMousedown(){
 function OnMouseup () {
     console.log("hello btn");
     this.style.backgroundColor = "#ADFF2F"
+    this.style.transitionDuration = "0.5s";
+}
+var button_2 = document.getElementsByClassName("fourth_button");
+// button це типу масив в який зібрав всі ноди (теги) з классом third-button
+for(var i=0; i<button_2.length; i++){
+
+    button_2[i].addEventListener("mouseover", Greeting_2);
+    button_2[i].addEventListener("mouseout",Back_2);
+    button_2[i].addEventListener("mousedown",OnMousedown_2);
+    button_2[i].addEventListener("mouseup",OnMouseup_2);
+
+// навішую на кожну ноду обработчик событий
+}
+function Greeting_2(){
+    console.log("hello btn");
+    this.style.backgroundColor = "black"
+   this.style.transitionDuration = "0.6s";
+
+}
+function Back_2(){
+    console.log("hello btn");
+    this.style.backgroundColor = "#d2d5da"
+   this.style.transitionDuration = "0.6s";
+}
+function OnMousedown_2(){
+    console.log("hello btn");
+    this.style.backgroundColor = "gray"
+    this.style.transitionDuration = "0.1s";
+}
+function OnMouseup_2 () {
+    console.log("hello btn");
+    this.style.backgroundColor = "black"
     this.style.transitionDuration = "0.5s";
 }
 // безпосердньо функції, які спрацьовують на 4 стани кнопки, this зберігає адресу на потрібну ноду (тег)
